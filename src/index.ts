aa5427012cf767c5a2e2096bec9abbda7d612c74
Irshad <muhammed.irshad@thinkpalm.com>
2024-02-01 14:28:18 +0530

    tuples

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-// Fundementals
-let sales = 123456;
-let sls:number = 123_567_67876;
-console.log(sls);
-let numbers = [1,2,3]
-let strs = ['1','2','3']
-console.log(numbers.forEach(n=>n.toString));
-console.log(strs.forEach(n=>n.replace));
-let level;
+//tuples
+let user: [number,string,number] = [1,'2',1];
